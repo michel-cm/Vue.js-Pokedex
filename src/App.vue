bac4bff407fba2a5ca9bc72df3fd2aca324c10cb
Michel Correa <58608300+michel-cm@users.noreply.github.com>
2023-02-07 14:14:12 -0300

    base home

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,46 @@
 <template>
   <div id="app">
-  
-  
+    <img
+      src="//hanashiro.github.io/pokedex/images/pokeball.svg"
+      alt=""
+      class="pokeball-back"
+    />
+    <input type="text" id="pokeFilter" placeholder="Search" />
+    <ul class="poke-list">
+      <li
+        v-for="(item, index) in pokemonList"
+        :key="index"
+        class="poke-list-item"
+      >
+        <img
+          :src="'//serebii.net/pokedex-xy/icon/' + item.number + '.png'"
+          alt=""
+        />
+        <span>{{ item.number }} - {{ item.name }}</span>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import "./css/styles.css";
 
 export default {
-  name: 'App',
-  components: {
-
-  }
-}
+  name: "App",
+  data() {
+    return {
+      pokemonList: [
+        { name: "Charizard 1", number: "006" },
+        { name: "Charizard 2", number: "007" },
+        { name: "Charizard 3", number: "008" },
+        { name: "Charizard 4", number: "009" },
+        { name: "Charizard 5", number: "448" },
+        { name: "Charizard 6", number: "655" },
+      ],
+    };
+  },
+  components: {},
+};
 </script>
 
-<style>
-#app {
-
-}
-</style>
+<style></style>
